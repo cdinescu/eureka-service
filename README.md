9a38e6f3d06f8aefb89249738bfcc8a61df341bf
Cristina Dinescu <crina91@gmail.com>
2020-05-19 21:31:36 +0300

    Fix Codacy - Part 2
    Signed-off-by: Cristina Dinescu <crina91@gmail.com>

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Vitanum: Eureka Service
 
 This is a service discovery server. Once the Kubernetes deployment is finished, it will be replaced by the ClusterIP service.
 However, it will be kept in order to facilitate service discovery for the 
-automatic unit tests [https://github.com/cdinescu/vitanum-deployment/tree/master/tests]
+automatic unit tests [GIT]: https://github.com/cdinescu/vitanum-deployment/tree/master/tests
 
 Technology stack:
-  - Spring Framework
-  - Spring Cloud
-  - Gradle
+- Spring Framework
+- Spring Cloud
+- Gradle
 
 <hr>
 
--- src/test/java/com/vitanum/eurekaservice/EurekaServiceApplicationTests.java
+++ src/test/java/com/vitanum/eurekaservice/EurekaServiceApplicationTests.java
@@ -1,17 +1,23 @@
 package com.vitanum.eurekaservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EurekaServiceApplicationTests {
 
+    @Value("${eureka.client.fetch-registry}")
+    private String fetchRegistry;
+
+    @Value("${eureka.client.register-with-eureka}")
+    private String registerWithEureka;
+
     @Test
     public void contextLoads() {
-        // the context successfully loaded
-        assertTrue(true);
+        assertEquals("false", fetchRegistry);
+        assertEquals("false", registerWithEureka);
     }
-
 }
